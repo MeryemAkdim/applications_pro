a66c79d58c8093058e55dc9794608060ffde3bb8
Meryem Akdim <makdim848@gmail.com>
2024-02-24 22:12:22 +0100

    Making the PhpUnit tests + installing the mockery package for testing

---
+++ database/factories/ApplicationFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Application>
+ */
+class ApplicationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'lname' => $this->faker->lastName,
+            'cv' => 'public/application_cv/factory/' . $this->faker->unique()->md5 . '.pdf',
+        ];
+    }
+}
